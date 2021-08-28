ceac95cd9ab82c434ccd852c9e99ea2058c38c1e
amit <amit212316@gmail.com>
2021-08-29 03:28:40 +0530

    canada jobs data scraping

---
+++ scrp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import ScrapData
+# Register your models here.
+admin.site.register(ScrapData)
---
+++ scrp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class ScrapData(models.Model):
+    objects = None
+    scrapped_url = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    empolyername = models.CharField(max_length=100)
+    empolyeremail = models.CharField(max_length=100)
